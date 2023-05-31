16e5cdc4d2f0e13f9224a14d5a459140ac4c8ab2
Aashishajmera <ajmeraaashu55@gmail.com>
2023-05-31 14:18:44 +0530

    add operator file

---
+++ operators/ArithmaticOp.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class ArithmaticOp {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 343;
+
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
