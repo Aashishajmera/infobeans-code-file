7840b858e05b77dcd5acbc3902f7e2d2a9f52ef2
Aashishajmera <ajmeraaashu55@gmail.com>
2023-06-23 11:11:23 +0530

    add Q1

--- javaNotepad/assignments/demoArrayAssignment-1/Q1InputOutput.java
+++ javaNotepad/assignments/demoArrayAssignment-1/Q1InputOutput.java
@@ -2,15 +2,34 @@ import java.util.Scanner;
 class Q1InputOutput{
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in);
-		System.out.pirntln("Enter Array of element: ");
+		System.out.println("Enter Array of length: ");
 		int n = sc.nextInt();
-		int arr[] = new int[n]
-		System.out.pirntln("Enter " +n +" element: ");
-		for(int i = 0; i < n; i++){
+		int arr[] = new int[n];
+		System.out.println("Enter " +n +" element: ");
+		int i,ans = -1;
+		for(i = 0; i < n; i++){
 			arr[i]=sc.nextInt();  //arr[i] is a index 
 		}
-		for(int j = 0; j < n; j++){
-			System.out.pirntln(arr[i]);  // print index value 
+		System.out.println("Entered number is: ");
+		
+		for(i = 0; i < n; i++){
+			System.out.println(arr[i]);  // print index value 
 		}
+		
+		System.out.println("Enter find number: ");
+		int find = sc.nextInt();
+		
+		for(i = 0; i < n; i++){
+			if(arr[i] == find){
+				ans = 1;
+			break;
+		}
+		}
+		if(ans == 1){
+			System.out.println("Yes you entered " +find +" number: ");
+		}else{
+			System.out.println("You not entered "+find +" number: ");
+		}
+
 	}
 }
