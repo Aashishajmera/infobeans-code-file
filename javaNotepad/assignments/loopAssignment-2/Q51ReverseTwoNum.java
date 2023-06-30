2863d0d46799af5a69608e3591234f315c45529a
Aashishajmera <ajmeraaashu55@gmail.com>
2023-06-30 19:00:44 +0530

    add Q50 and 51

--- javaNotepad/assignments/loopAssignment-2/Q51ReverseTwoNum.java
+++ javaNotepad/assignments/loopAssignment-2/Q51ReverseTwoNum.java
@@ -1,22 +1,19 @@
-// 50) WAP to find out all the palindrome numbers between two entered numbers
+// 51) WAP to reverse all the numbers between two entered numbers
 
 import java.util.Scanner;
-class Q50PalindromeTwoNum{
+class Q51ReverseTwoNum{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first number: ");
 		int f = sc.nextInt();
 		System.out.println("Enter second number: ");
 		int s = sc.nextInt();
-		int rem = 0, rev = 0,j ;
+		int rem = 0, rev = 0;
 		for(int i = f; i <= s; i++){
-			for(j = i; j != 0; j = j/10){
+			for(int j = i; j!=0; j = j/10){
 				rem = j%10;
-				rev = (rev*10) + rem;
-				 
-			}if(i == rev){
-				System.out.println("Palindrom num is: " +rev);
-			}
+				rev = rev*10 + rem;
+			}System.out.println(rev);
 			rev = 0;
 			rem = 0;
 		}
---
+++ javaNotepad/assignments/loopAssignment-2/Q52ArmstrongTwoNum.java
@@ -0,0 +1,5 @@
+// 52) WAP to find out all the Armstrong numbers between two entered numbers
+
+class Q52ArmstrongTwoNum{
+	
+}
