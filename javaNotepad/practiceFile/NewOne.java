7f66acdd33a2e28e0ba66399c140e768651df85c
Aashishajmera <ajmeraaashu55@gmail.com>
2023-07-12 17:45:11 +0530

    name change Q3

--- javaNotepad/practiceFile/NewOne.java
+++ javaNotepad/practiceFile/NewOne.java
@@ -1,21 +1,24 @@
+/*
+1
+2 11
+3 10 12
+4 9 13 18
+5 8 14 17 19
+6 7 15 16 20 21 */
+
+import java.util.Scanner;
 class NewOne{
 	public static void main(String[] args){
-		int k = 9, l = 12; 
-		for(int r = 1; r <= 6; r++){
-			for(int c = 1; c <= r; c++){
-				if(r >= 2 && r <= 6){
-					if(c%2 == 0 && c >= 2){
-						System.out.print(r+k+" ");
-						k-=2; l-=2;
-					}else if(c % 2 != 0 && c >= 2){
-						System.out.print(r+l +" ");
-						k-=2; l-=2;
-					}else{
-						System.out.print(r+" ");
-					}
-				}else{
-					System.out.print(r +" ");
-				}
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter row: ");
+		int n = sc.nextInt();
+		
+		for(int  r = 1; r <= n; r++){
+			System.out.print(r +" ");
+			int val = r;
+			for(int c = 1; c < r; c++){
+				val = (c%2 != 0) ?val+((2*(n+1-r))-1):val+(2*(r-c));
+				System.out.print( val + " ");
 			}
 			System.out.println();
 		}
