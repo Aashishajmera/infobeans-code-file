623820fbdf323073ee7515f77fb7e84afc402ff2
Aashishajmera <ajmeraaashu55@gmail.com>
2023-07-21 23:44:06 +0530

    add polymorphism var-args

---
+++ javaNotepad/OOPS/Polymorphism/methodOverloading/MyPractice.java
@@ -0,0 +1,40 @@
+class Test{
+	// create method:
+	
+	/* public void m1(){
+		System.out.println("m1 without perameter: ");
+	}
+	
+	public void add(int... x){
+		int sum = 0;
+		System.out.println("m1 with var-argument's: ");
+		for(int i = 0; i < x.length; i++){
+			sum += x[i];
+			System.out.println(x[i]);
+		}
+		System.out.println("The sum is: "+sum);
+	} */
+	
+	public void m1(int... x){
+		System.out.println("int var-argument: "+x);
+	}
+	
+	public void m1(long... x){
+		System.out.println("m1 with var-argument: " +x);
+	}
+	
+	public void m1(long x){
+		System.out.println("long " +x);
+	}
+
+}
+
+
+class MyPractice{
+	
+	public static void main(String[] args){
+		Test obj = new Test();
+		obj.m1(99);
+		obj.m1(3l,5);
+	}
+}
--- javaNotepad/method/methodInOneCode.java
+++ javaNotepad/method/methodInOneCode.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 class mthd{
-	public void m1(){
+	
+	/*public void m1(){
 		System.out.println("m1 called: ");
 		m3();
 	}
@@ -13,12 +16,70 @@ class mthd{
 		m2();
 	}
 	
+	
+	
+	// Method for array: 
+	
+	public int[] arrM(int arr[]){
+		System.out.println("Enter array element is: ");
+		
+		return arr;
+	}*/
+	
+	
+	
+	
+	// method access A and B 
+	
+	public void a(){
+		System.out.println("a method: ");
+	}
+	
 }
+
+
+// second class
+class mthd2{
+		public void b(mthd obj){
+			System.out.println("b method: ");
+			obj.a();
+		}
+	}
 
 
 class methodInOneCode{
 	public static void main(String[] args){
+		
+	/* 	//array 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of array: ");
+		int size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		System.out.println("Enter " +size +" element: ");
+		
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = sc.nextInt();
+		}
+		
 		mthd obj = new mthd();
-		obj.m1();
+		
+		
+		// obj.m1();
+		int x[] = obj.arrM(arr);
+		
+		for(int i = 0; i < x.length; i++){
+			System.out.println(x[i]);
+		} */
+		
+		
+		
+		//object 
+		mthd2 obj = new mthd2();
+		mthd obj1 = new mthd();
+		
+		obj.b(obj1);
+		
+		
 	}
 }
