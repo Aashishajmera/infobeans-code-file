f006124df4022cefc437b1cf82006d836c6f5757
Aashishajmera <ajmeraaashu55@gmail.com>
2023-07-21 23:45:12 +0530

    add 1Darray code

---
+++ javaNotepad/assignments/1DarrayAssConvertMethod/1D-firstMethod/Q1Sum2.java
@@ -0,0 +1,7 @@
+// 1. WAP sum of array element 
+
+class Q1Sum2{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+	}
+}
--- javaNotepad/assignments/loopAssignment-2-Method/Q8Fibonacci.java
+++ javaNotepad/assignments/loopAssignment-2-Method/Q8Fibonacci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Q8Fibonacci {
 
     // create method:
-    public void fibonacci(int n) {
+    public void fibonacci(int n) { 
         int a = 0, b = 1, c = 0;
         for (int i = 1; i <= n; i++) {
             a = b;
