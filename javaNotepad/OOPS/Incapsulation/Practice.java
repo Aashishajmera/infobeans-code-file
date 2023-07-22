4da3be6497695aa32b45173d78a062b7f2d36b49
Aashishajmera <ajmeraaashu55@gmail.com>
2023-07-22 13:08:11 +0530

    update practice file

--- javaNotepad/OOPS/Incapsulation/Practice.java
+++ javaNotepad/OOPS/Incapsulation/Practice.java
@@ -24,18 +24,21 @@ class Practice{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		Demo obj = new Demo();
-		Demo obj1 = obj; // obj ki properties ko apne me leleta hai
+		// Demo obj1 = obj; // obj ki properties ko apne me leleta hai
 		// Demo obj1 = new Demo();
 		/* obj.m1();
 		obj.setK(sc);
 		obj.m1();
 		obj1.m1(); */
 		
-		Demo1  ojjjj = obj;
-		ojjjj.m1();
+		/* Demo1  ojjjj = obj;
+		ojjjj.m1();              esa nhi kr sakte hai because class are different*/
 		
-		// obj.k;
-		System.out.println(obj.k= 5);
+		obj.k=99;  // variable private nhi hai ishlie value change kr di 
+		System.out.println(obj.k= 5); // or ese print bhi krwa sakte hai 
+		
+		
+		// but private variable hote to method se value change krna hota without method nhi kr sakte setK method ko call krna pdta or access krne ke lie get method:
 		// obj.m1();
 	}
 }
