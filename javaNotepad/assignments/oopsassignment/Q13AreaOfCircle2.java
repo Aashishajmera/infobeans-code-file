c36508de7578e96209f39c259f7d95378bfdc445
Aashishajmera <ajmeraaashu55@gmail.com>
2023-07-28 06:50:29 +0530

    add recursion and areaOfCircle code

---
+++ javaNotepad/assignments/oopsassignment/Q13AreaOfCircle2.java
@@ -0,0 +1,23 @@
+/* 13) Create a class to calculate Area of circle with one data member to store the radius and another to store area value. Create method members
+init - to input radius from user
+calc - to calculate area
+display- to display area */
+
+import java.util.Scanner;
+class AreaOfCircle{
+	private float radius, areaValue;
+	
+	//create default constructor:
+	public AreaOfCircle(){
+		
+	}
+	
+	//create constructor:
+	
+}
+
+class Q13AreaOfCircle2{
+	public static void main(String[] args){
+		Scanner sc = new 
+	}
+}
---
+++ javaNotepad/dataType/Booln.java
@@ -0,0 +1,6 @@
+class Booln{
+	private static boolean flag;
+	public static void main(String[] args){
+		System.out.println(flag);
+	}
+}
