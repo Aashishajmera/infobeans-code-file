91b6b7a684c4cadd890b6d9275caa48437e18be0
Aashishajmera <ajmeraaashu55@gmail.com>
2023-07-30 21:01:26 +0530

    update Question-01

--- javaprogrammingpractice/28 july/Q1Information2.java
+++ javaprogrammingpractice/28 july/Q1Information2.java
@@ -1,54 +1,61 @@
 import java.util.Scanner;
-class Employee{
-	String name,address;
+
+class Employee {
+	String name, address;
 	int year;
-	public void firstEmployee(String name, String address ,int year){
+
+	// create default constructor:
+	public Employee() {
+
+	}
+
+	// create constructor:
+	public Employee(String name, String address, int year) {
 		this.name = name;
 		this.year = year;
 		this.address = address;
 	}
-	public void secondEmployee(String name1, String address1, int year1){
-		this.name = name1;
-		this.year = year1;
-		this.address = address1;
+
+	// formate:
+	public void formate() {
+		System.out.println("Name: \t\t\t\tAddress: \t\t\tYear:");
+
 	}
-	public void thirdEmployee(String name2, String address2, int year2){
-		this.name = name2;
-		this.year = year2; 
-		this.address = address2;
-	}
-	
-	public void display(){
-		
+
+	// display the output:
+	public void display() {
+		System.out.println(this.name + "\t\t\t\t" + this.address + "\t\t\t\t" + this.year);
+
 	}
 }
 
-class Q1Information2{
-	public static void main(String[] args){
+class Q1Information2 {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Enter first employee name , address, year of joining,  ");
 		String name1 = sc.nextLine();
 		String address1 = sc.nextLine();
 		int year1 = sc.nextInt();
-		
-		System.out.println("Enter second employee name , address, year of joining,  ");
-		String name2 = sc.nextLine();
-		String address2 = sc.nextLine();
-		int year2 = sc.nextInt();
-		
-		System.out.println("Enter third employee name , address, year of joining,  ");
-		String name3 = sc.nextLine();
-		String address3 = sc.nextLine();
-		int year3 = sc.nextInt();
-		
-		Employee obj1 = new Employee();
-		Employee obj2 = new Employee();	
-		Employee obj3 = new Employee();
+		sc.nextLine();
+		Employee obj1 = new Employee(name1, address1, year1);
+		System.out.println("Enter first employee name , address, year of joining,  ");
+		name1 = sc.nextLine();
+		address1 = sc.nextLine();
+		year1 = sc.nextInt();
+		sc.nextLine();
+		Employee obj2 = new Employee(name1, address1, year1);
 
-		
-		// first employee:
-		obj1.firstEmployee(name1, address1, year1);
-		obj2.secondEmployee(name2, address2, year2);
-		obj3.thirdEmployee(name3, address3, year3);
+		System.out.println("Enter first employee name , address, year of joining,  ");
+		name1 = sc.nextLine();
+		address1 = sc.nextLine();
+		year1 = sc.nextInt();
+		Employee obj3 = new Employee(name1, address1, year1);
+
+		obj1.formate();
+		obj1.display();
+		obj2.display();
+		obj3.display();
+	
 	}
 }
