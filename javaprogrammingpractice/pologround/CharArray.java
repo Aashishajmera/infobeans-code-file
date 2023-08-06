3763ba9abd659602bf602d8dd95c5f38a5ae9fc7
Aashishajmera <ajmeraaashu55@gmail.com>
2023-08-07 01:00:40 +0530

    add more code

---
+++ javaprogrammingpractice/pologround/CharArray.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class CharArray{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch[] = {'a', 'a', 's', 'h', 'i', 's', 'h'};
+        // for(int i = 0; i < ch.length; i++){
+        //     System.out.print(ch[i] +" ");
+        // }
+        String s = new String(ch);
+        System.out.println(s);
+    }
+}
---
+++ javaprogrammingpractice/pologround/St.java
@@ -0,0 +1,6 @@
+class St{
+	public static void main(String[] args){
+		String s1 = new "Hello";
+		System.
+	}
+}
