d9356e217d363fbb798643ada91d687d6023b37e
Aashishajmera <ajmeraaashu55@gmail.com>
2023-09-13 13:19:47 +0530

    add demo 11 code file

---
+++ javaNetbeans/Basic/src/Demo11.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Demo11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (n == 1) {
+                        System.exit(1);
+                    }
+                }
+                
+                
+                
+                System.out.println("2");
+            }
+            System.out.println("1");
+        }
+    }
+}
