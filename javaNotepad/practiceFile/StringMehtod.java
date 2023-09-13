0bfe31919efa22029d8ecc889e4722148e44d483
Aashishajmera <ajmeraaashu55@gmail.com>
2023-09-13 13:16:49 +0530

    add string method code file

---
+++ javaNotepad/practiceFile/StringMehtod.java
@@ -0,0 +1,18 @@
+public class StringMehtod {
+
+  public static void main(String[] args) {
+    // final String s1 = "Hello";
+    // String s2 = s1 + "world";
+    // String s3 = "HelloWorld";
+    // String s4 = "Hello"+"World";
+    // System.out.println(s2 == s3);
+    // System.out.println(s3 == s4);
+
+    // System.out.println(s2);
+    // System.out.println(s1 == s2);
+    // System.out.println(s2 == s3);
+    
+
+    
+  }
+}
