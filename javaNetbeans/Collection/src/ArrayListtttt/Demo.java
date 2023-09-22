102b2111aa8bf9d81d27bf6efe175d4e411daadf
Aashishajmera <ajmeraaashu55@gmail.com>
2023-09-22 07:23:49 +0530

    add arraylist code

---
+++ javaNetbeans/Collection/src/ArrayListtttt/Demo.java
@@ -0,0 +1,35 @@
+
+package ArrayListtttt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList list = new ArrayList(3);
+        System.out.println("Enter 5 element");
+//        for(int i = 0; i < 5; i++){
+////            list.add(i) = sc.nextInt();
+//        }
+       
+//add value: 
+list.add("0"+10);
+list.add(20);
+list.add(30);
+list.add(40);
+list.add("Aashish");
+
+//for(int i = 0; i < list.size(); i++){
+//    System.out.print(list.get(i)+" ");
+//}
+
+        System.out.println();
+        
+     
+// get value: 
+//System.out.println(list.get(0));
+       
+    }
+}
