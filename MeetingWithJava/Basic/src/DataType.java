973cc9abcb24f3bb57e364c0580679d62550986a
Aashishajmera <ajmeraaashu55@gmail.com>
2023-11-06 11:28:11 +0530

    rename html folder

---
+++ MeetingWithJava/Basic/src/DataType.java
@@ -0,0 +1,8 @@
+public class DataType {
+    public static void main(String[] args) {
+//        System.out.println(7.6%0);
+//        System.out.println(7.5/0);
+//        long l = 2147483648;
+//        System.out.println(l);
+    }
+}
