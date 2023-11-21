b29d9d3bc35dc7bd7c34b8eb39bcae590c494a9e
Aashishajmera <ajmeraaashu55@gmail.com>
2023-11-21 15:13:07 +0530

    20 november meeting code added

---
+++ MeetingWithJava/20_Nov/Pt2.java
@@ -0,0 +1,81 @@
+// @ @ @ @ @
+//   @ @ @ @
+//     @ @ @
+//       @ @
+// 	       @
+
+
+public class Pt2 {
+
+  public static void main(String[] args) {
+    // int k = 0;
+    // for(int i = 1; i <= 5; i++){
+    //     for(int j = 0; j < k; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j = 5; j > k; j--){
+    //         System.out.print("@");
+    //     }
+    //     k++;
+    //     System.out.println();
+    // }
+
+    // for(int i = 0; i < 5; i++){
+    //     for(int j = 0; j < i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j = 5; j > i; j--){
+    //         System.out.print("@");
+    //     }
+    //     System.out.println();
+    // }
+
+    // for (int i = 1; i <= 5; i++) {
+    //   for (int j = 5; j > 6 - i; j--) System.out.print(" ");
+    //   for (int j = i; j <= 5; j++) System.out.print("@");
+    //   System.out.println();
+    // }
+
+    // ===========================================CONDITION=========================================
+    // @ @ @ @ @
+    //   @ @ @ @
+    //     @ @ @
+    //       @ @
+    // 	       @
+
+
+    // int k = 0;
+    // for (int i = 1; i <= 5; i++) {
+    //   for (int j = 1; j <= 5; j++) {
+    //     if (j <= k) {
+    //       System.out.print(" ");
+    //       k++;
+    //     } else {
+    //       System.out.print("@");
+    //     }
+    //   }
+    //  System.out.println();
+    // }
+
+
+    // for (int i = 0; i < 5; i++) {
+    //   for (int j = 1; j <= 5; j++) {
+    //     if (j <= i) System.out.print(" "); else System.out.print("@");
+    //   }
+    //   System.out.println();
+    // }
+
+
+    // for (int i = 0; i < 5; i++) {
+    //   for (int j = 1; j <= 5; j++) {
+    //     String k = (j <= i) ? " " : "@";
+    //     System.out.print(k);
+    //   }
+    //   System.out.println();
+    // }
+
+
+
+
+  }
+}
