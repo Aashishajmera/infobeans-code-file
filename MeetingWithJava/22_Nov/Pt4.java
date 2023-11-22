54cf6c6829bb5fbee277b8983fe6c791cc9cbcb7
Aashishajmera <ajmeraaashu55@gmail.com>
2023-11-22 22:16:02 +0530

    22 Nov meeting code added

---
+++ MeetingWithJava/22_Nov/Pt4.java
@@ -0,0 +1,14 @@
+//     *
+//     *
+//   * * *
+//   * * *
+// * * * * *
+// * * * * *
+
+public class Pt4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 6; i++){
+            
+        }
+    }
+}
